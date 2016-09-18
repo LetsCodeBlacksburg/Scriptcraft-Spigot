12c2937b298bfd8772004f5eae54ce01a1267f3b
Nathan Liles <nml5566@vt.edu>
2016-09-17 22:51:05 -0400

    Changed code to match slides

--- code/fireballSword.js
+++ code/fireballSword.js
@@ -15,41 +15,41 @@ events.playerInteract( onArmSwing );
 
 function isFireballSword( item ) {
   var luckLevel = item.getEnchantmentLevel(bkEnchant.LUCK)
-  if (item.getType() == items.diamondSword() && luckLevel == 3 ) 
-  {
+  if (item.getType() == items.diamondSword() && luckLevel == 3 ) {
     return true;
   }
   return false;
 }
 
 function shootFireball(player) {
-  var eyeLoc = player.getEyeLocation();
-  var fireball = player.location.getWorld().spawnEntity(eyeLoc, entities.fireball());
+  var entityType = entities.fireball();
+  var eyeLevel = player.getEyeLocation();
+  var world = player.getWorld();
+  var fireball = world.spawnEntity(eyeLevel, entityType);
   fireball.setYield(0); /* prevent fireball from destroying blocks */
-  fireball.setVelocity(eyeLoc.getDirection().normalize().multiply(2));
-  fireball.setShooter(player);
+  fireball.setVelocity(eyeLevel.getDirection().multiply(5));
+  fireball.setShooter(player); /* make fireball ours so it won't explode us */
 }
 
-function addRecipe() {
-  var item = items.diamondSword(1);
+var item = items.diamondSword(1);
 
-  var meta = item.getItemMeta();
-  meta.setDisplayName("Fireball Sword");
-  item.setItemMeta(meta);
-  item.addUnsafeEnchantment(bkEnchant.LUCK, 3);
+var meta = item.getItemMeta();
+meta.setDisplayName("Fireball Sword");
+item.setItemMeta(meta);
+item.addUnsafeEnchantment(bkEnchant.LUCK, 3);
 
-  var fireballSwordRecipe = new Object();
-  fireballSwordRecipe.result = item;
-  fireballSwordRecipe.shape = [ 'B',
-				'B',
-				'S' ];
-  fireballSwordRecipe.ingredients = {
-    B: items.blazeRod(1), 
-    S: items.stick(1)
-  };
+var recipe = new Object();
+recipe.result = item;
+recipe.shape = [ 'BDB',
+		 'BDB',
+		 'BSB' ];
+recipe.ingredients = {
+  B: items.blazeRod(1), 
+  D: items.diamond(1), 
+  S: items.stick(1)
+};
 
-  var recipes = require('recipes');
-  var recipe = recipes.add( fireballSwordRecipe );
-}
+var recipes = require('recipes');
+recipes.add( recipe );
 
 addRecipe();
