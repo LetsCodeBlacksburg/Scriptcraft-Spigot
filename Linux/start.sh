8543dfdf2f9506f2f22ca9bebe7359dec213779d
Nathan Liles <nml5566@vt.edu>
2017-02-21 12:34:22 -0500

    Start scripts point to new version

--- Linux/start.sh
+++ Linux/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh  
 BINDIR=$(dirname "$(readlink -fn "$0")")  
 cd "$BINDIR"  
-java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-1.10.2-R0.1-SNAPSHOT-latest.jar
+java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-1.11.2.jar
--- Windows/start.bat
+++ Windows/start.bat
@@ -1,3 +1,3 @@
 @echo off  
-java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-1.10.2-R0.1-SNAPSHOT-latest.jar
+java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-1.11.2.jar
 pause
